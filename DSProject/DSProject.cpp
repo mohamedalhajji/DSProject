1b844e5af9e8e79c4bdc4d4c998513276a4c4662
mohamedalhajji <mohamedalhajji@hotmail.com>
2023-12-12 19:37:56 +0300

    added detailed comments for clarity and documentation

--- DSProject/DSProject.cpp
+++ DSProject/DSProject.cpp
@@ -1,8 +1,9 @@
 #include <vector>
 #include <string>
+#include <iostream>
 using namespace std;
 
-// A simple primary hash function for strings
+// Primary hash function: Sums ASCII values of characters in the key and mods by table size
 unsigned int hash1(const string& key, int tableSize) {
     unsigned int hashVal = 0;
     for (char ch : key) {
@@ -11,7 +12,7 @@ unsigned int hash1(const string& key, int tableSize) {
     return hashVal % tableSize;
 }
 
-// A secondary hash function for double hashing
+// Secondary hash function for double hashing: Computes a secondary hash to avoid clustering
 unsigned int hash2(const string& key, int tableSize) {
     unsigned int hashVal = 0;
     for (char ch : key) {
@@ -20,23 +21,26 @@ unsigned int hash2(const string& key, int tableSize) {
     return (tableSize - 1) - (hashVal % (tableSize - 1));
 }
 
+// HashNode class: Represents an individual node in the hash table with a key, value, and deletion flag.
 template<typename K, typename V>
 class HashNode {
 public:
-    K key;
-    V value;
-    bool isDeleted;
+    K key;            // Key of the hash node
+    V value;         // Value of the hash node
+    bool isDeleted; // Flag to mark the node as logically deleted
 
+    // Constructor: Initializes the key, value, and sets isDeleted flag to false
     HashNode(K key, V value) : key(key), value(value), isDeleted(false) {}
 };
 
+// HashTable class represents the hash table itself.
 template<typename K, typename V>
 class HashTable {
 private:
-    vector<HashNode<K, V>*> table;
-    int capacity;
-    int size;
-    bool isRehashing;
+    vector<HashNode<K, V>*> table;  // Dynamic array for the hash table
+    int capacity;                  // Current capacity of the hash table
+    int size;                     // Number of elements in the hash table
+    bool isRehashing;            // Flag to check if rehashing is in progress
     int nextPrime(int n) {
         if (n <= 1) return 2;
         if (n <= 3) return 3;
@@ -56,6 +60,7 @@ private:
         }
     }
 
+    // Computes the hash index for a given key and probe sequence
     unsigned int getHashIndex(const K& key, int probe) {
         int hashIndex1 = hash1(key, capacity);
         int hashIndex2 = hash2(key, capacity);
@@ -63,11 +68,14 @@ private:
     }
 
 public:
+    // Constructor: Initializes the hash table with a given capacity and default values
     HashTable(int capacity) : capacity(capacity), size(0), isRehashing(false) {
         table.resize(capacity, nullptr);
     }
 
+    // Insert method: Inserts a key-value pair into the hash table
     void insert(const K& key, const V& value) {
+        // Check for rehashing need before insertion
         if (!isRehashing && static_cast<double>(size) / capacity > 0.7) {
             rehash();
         }
@@ -90,29 +98,33 @@ public:
         } while (probe < capacity);
     }
 
+    // Search method: Finds and returns the value associated with a given key
     V search(const K& key) {
-        int probe = 0;
+        int probe = 0; // Probing counter
         int hashIndex;
+        // Continuously probe until the key is found or an empty slot is encountered
         while (probe < capacity) {
             hashIndex = getHashIndex(key, probe);
             if (table[hashIndex] == nullptr)
-                throw runtime_error("Key not found");
+                throw runtime_error("Key not found"); // Key not in table
             if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key)
-                return table[hashIndex]->value;
+                return table[hashIndex]->value; // Return found value
             probe++;
         }
-        throw runtime_error("Key not found");
+        throw runtime_error("Key not found"); // Key not found after full probe
     }
 
+    // Remove method: Logically deletes a key-value pair from the hash table
     void remove(const K& key) {
-        int probe = 0;
+        int probe = 0; // Probing counter
         int hashIndex;
+        // Probe to find the key
         while (probe < capacity) {
             hashIndex = getHashIndex(key, probe);
             if (table[hashIndex] == nullptr)
-                return;
+                return; // Key not in table
             if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key) {
-                table[hashIndex]->isDeleted = true;
+                table[hashIndex]->isDeleted = true; // Mark as deleted
                 size--;
                 return;
             }
@@ -120,27 +132,31 @@ public:
         }
     }
 
+    // Destructor: Cleans up the dynamic memory used by the hash table
     ~HashTable() {
         for (auto& node : table) {
             if (node != nullptr) {
-                delete node;
+                delete node; // Delete each node
             }
         }
     }
 
+    // Rehash method: Resizes the table and rehashes all elements when load factor is high
     void rehash() {
         isRehashing = true;
+        // Create a new larger table
         vector<HashNode<K, V>*> oldTable = table;
         table = vector<HashNode<K, V>*>(nextPrime(2 * capacity), nullptr);
-        size = 0;
+        size = 0; // Reset size
         capacity = table.size();
 
+        // Re-insert elements from old table
         for (auto node : oldTable) {
             if (node != nullptr && !node->isDeleted) {
                 insert(node->key, node->value);
-                delete node;
+                delete node; // Delete old node
             }
         }
-        isRehashing = false;
+        isRehashing = false; // Sets isDeleted flag back to false
     }
 };
