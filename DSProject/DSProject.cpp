ceb0181618ce18e1993bd8ddcfca3a3be665c63e
mohamedalhajji <mohamedalhajji@hotmail.com>
2023-12-12 18:12:19 +0300

    Changed hashing method to double hashing

--- DSProject/DSProject.cpp
+++ DSProject/DSProject.cpp
@@ -1,10 +1,9 @@
 #include <vector>
-#include <list>
 #include <string>
 using namespace std;
 
-// A simple hash function for strings
-unsigned int HashFunction(const string& key, int tableSize) {
+// A simple primary hash function for strings
+unsigned int hash1(const string& key, int tableSize) {
     unsigned int hashVal = 0;
     for (char ch : key) {
         hashVal += ch;
@@ -12,14 +11,23 @@ unsigned int HashFunction(const string& key, int tableSize) {
     return hashVal % tableSize;
 }
 
+// A secondary hash function for double hashing
+unsigned int hash2(const string& key, int tableSize) {
+    unsigned int hashVal = 0;
+    for (char ch : key) {
+        hashVal = 37 * hashVal + ch;
+    }
+    return (tableSize - 1) - (hashVal % (tableSize - 1));
+}
+
 template<typename K, typename V>
 class HashNode {
 public:
     K key;
     V value;
-    HashNode* next;
+    bool isDeleted;
 
-    HashNode(K key, V value) : key(key), value(value), next(nullptr) {}
+    HashNode(K key, V value) : key(key), value(value), isDeleted(false) {}
 };
 
 template<typename K, typename V>
@@ -29,9 +37,11 @@ private:
     int capacity;
     int size;
 
-    // Hash function as defined earlier
-    unsigned int getHashIndex(const K& key) {
-        return hashFunction(key, capacity);
+    // Adjusted for double hashing
+    unsigned int getHashIndex(const K& key, int probe) {
+        int hashIndex1 = hash1(key, capacity);
+        int hashIndex2 = hash2(key, capacity);
+        return (hashIndex1 + probe * hashIndex2) % capacity;
     }
 
 public:
@@ -40,95 +50,62 @@ public:
     }
 
     void insert(const K& key, const V& value) {
-        void HashTable<K, V>::insert(const K & key, const V & value) {
-            unsigned int hashIndex = getHashIndex(key);
-            HashNode<K, V>* prev = nullptr;
-            HashNode<K, V>* entry = table[hashIndex];
-
-            while (entry != nullptr && entry->key != key) {
-                prev = entry;
-                entry = entry->next;
-            }
-
-            if (entry == nullptr) {
-                entry = new HashNode<K, V>(key, value);
-                if (prev == nullptr) {
-                    // Insert as first bucket
-                    table[hashIndex] = entry;
-                }
+        int probe = 0;
+        int hashIndex;
+        do {
+            hashIndex = getHashIndex(key, probe);
+            if (table[hashIndex] == nullptr || table[hashIndex]->isDeleted) {
+                if (table[hashIndex] == nullptr)
+                    table[hashIndex] = new HashNode<K, V>(key, value);
                 else {
-                    prev->next = entry;
+                    table[hashIndex]->key = key;
+                    table[hashIndex]->value = value;
+                    table[hashIndex]->isDeleted = false;
                 }
+                size++;
+                return;
             }
-            else {
-                // Just update the value
-                entry->value = value;
-            }
-            size++;
-        }
-
+            probe++;
+        } while (probe < capacity);
     }
 
     V search(const K& key) {
-        V HashTable<K, V>::search(const K & key) {
-            unsigned int hashIndex = getHashIndex(key);
-            HashNode<K, V>* entry = table[hashIndex];
-
-            while (entry != nullptr) {
-                if (entry->key == key) {
-                    return entry->value;
-                }
-                entry = entry->next;
-            }
-
-            // Key not found, handle accordingly
-            throw std::runtime_error("Key not found");
+        int probe = 0;
+        int hashIndex;
+        while (probe < capacity) {
+            hashIndex = getHashIndex(key, probe);
+            if (table[hashIndex] == nullptr)
+                throw std::runtime_error("Key not found");
+            if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key)
+                return table[hashIndex]->value;
+            probe++;
         }
-
+        throw std::runtime_error("Key not found");
     }
 
     void remove(const K& key) {
-        void HashTable<K, V>::remove(const K & key) {
-            unsigned int hashIndex = getHashIndex(key);
-            HashNode<K, V>* entry = table[hashIndex];
-            HashNode<K, V>* prev = nullptr;
-
-            while (entry != nullptr && entry->key != key) {
-                prev = entry;
-                entry = entry->next;
-            }
-
-            if (entry == nullptr) {
-                // Key not found
+        int probe = 0;
+        int hashIndex;
+        while (probe < capacity) {
+            hashIndex = getHashIndex(key, probe);
+            if (table[hashIndex] == nullptr)
+                return;
+            if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key) {
+                table[hashIndex]->isDeleted = true;
+                size--;
                 return;
             }
-
-            if (prev == nullptr) {
-                // Remove first bucket
-                table[hashIndex] = entry->next;
-            }
-            else {
-                prev->next = entry->next;
-            }
-
-            delete entry;
-            size--;
+            probe++;
         }
-
     }
 
     // Destructor to clean up the table
     ~HashTable() {
-        HashTable<K, V>::~HashTable() {
-            for (HashNode<K, V>* headNode : table) {
-                while (headNode != nullptr) {
-                    HashNode<K, V>* prev = headNode;
-                    headNode = headNode->next;
-                    delete prev;
-                }
+        for (auto& node : table) {
+            if (node != nullptr) {
+                delete node;
             }
         }
-
     }
 
     // Other methods like resize/rehashing can be added here
