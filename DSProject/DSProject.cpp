e839a035aff5fe4a8725f22905fa089853357430
mohamedalhajji <mohamedalhajji@hotmail.com>
2023-12-12 18:27:03 +0300

    fixed double rehash
    fixed the 2nd rehash members, also added a function that checks if prime

--- DSProject/DSProject.cpp
+++ DSProject/DSProject.cpp
@@ -99,7 +99,6 @@ public:
         }
     }
 
-    // Destructor to clean up the table
     ~HashTable() {
         for (auto& node : table) {
             if (node != nullptr) {
@@ -108,5 +107,99 @@ public:
         }
     }
 
-    // Other methods like resize/rehashing can be added here
+    void rehash() {
+        vector<HashNode<K, V>*> oldTable = table;
+        table = vector<HashNode<K, V>*>(nextPrime(2 * capacity), nullptr);
+        size = 0;
+        capacity = table.size();
+
+        for (auto node : oldTable) {
+            if (node != nullptr && !node->isDeleted) {
+                insert(node->key, node->value);
+                delete node;
+            }
+        }
+    }
+
+    void insert(const K& key, const V& value) {
+
+        if (static_cast<double>(size) / capacity > 0.7) {
+            rehash();
+        }
+
+        int probe = 0;
+        int hashIndex;
+        do {
+            hashIndex = getHashIndex(key, probe);
+            if (table[hashIndex] == nullptr || table[hashIndex]->isDeleted) {
+                if (table[hashIndex] == nullptr)
+                    table[hashIndex] = new HashNode<K, V>(key, value);
+                else {
+                    table[hashIndex]->key = key;
+                    table[hashIndex]->value = value;
+                    table[hashIndex]->isDeleted = false;
+                }
+                size++;
+                return;
+            }
+            probe++;
+        } while (probe < capacity);
+    }
+
+    V search(const K& key) {
+        int probe = 0;
+        int hashIndex;
+        while (probe < capacity) {
+            hashIndex = getHashIndex(key, probe);
+            if (table[hashIndex] == nullptr)
+                throw std::runtime_error("Key not found");
+            if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key)
+                return table[hashIndex]->value;
+            probe++;
+        }
+        throw std::runtime_error("Key not found");
+    }
+
+    void remove(const K& key) {
+        int probe = 0;
+        int hashIndex;
+        while (probe < capacity) {
+            hashIndex = getHashIndex(key, probe);
+            if (table[hashIndex] == nullptr)
+                return;
+            if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key) {
+                table[hashIndex]->isDeleted = true;
+                size--;
+                return;
+            }
+            probe++;
+        }
+    }
+
+    ~HashTable() {
+        for (auto& node : table) {
+            if (node != nullptr) {
+                delete node;
+            }
+        }
+    }
+
+    int nextPrime(int n) {
+        if (n <= 1) return 2;
+        if (n <= 3) return 3;
+
+        if (n % 2 == 0) n++;
+
+        while (true) {
+            bool isPrime = true;
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) return n;
+            n += 2;
+        }
+    }
 };
