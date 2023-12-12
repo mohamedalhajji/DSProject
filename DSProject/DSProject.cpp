b6c3983a9eae62afe06bb9300eadd8ced8947555
mohamedalhajji <mohamedalhajji@hotmail.com>
2023-12-12 19:11:50 +0300

    clean up with new additions
    fixed the prime function, added a rehashing member to not rehash while hashing, removed redundant members that were defined twice

--- DSProject/DSProject.cpp
+++ DSProject/DSProject.cpp
@@ -36,6 +36,25 @@ private:
     vector<HashNode<K, V>*> table;
     int capacity;
     int size;
+    bool isRehashing;
+    int nextPrime(int n) {
+        if (n <= 1) return 2;
+        if (n <= 3) return 3;
+
+        if (n % 2 == 0) n++;
+
+        while (true) {
+            bool isPrime = true;
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) return n;
+            n += 2;
+        }
+    }
 
     unsigned int getHashIndex(const K& key, int probe) {
         int hashIndex1 = hash1(key, capacity);
@@ -44,11 +63,14 @@ private:
     }
 
 public:
-    HashTable(int capacity) : capacity(capacity), size(0) {
+    HashTable(int capacity) : capacity(capacity), size(0), isRehashing(false) {
         table.resize(capacity, nullptr);
     }
 
     void insert(const K& key, const V& value) {
+        if (!isRehashing && static_cast<double>(size) / capacity > 0.7) {
+            rehash();
+        }
         int probe = 0;
         int hashIndex;
         do {
@@ -107,6 +129,7 @@ public:
     }
 
     void rehash() {
+        isRehashing = true;
         vector<HashNode<K, V>*> oldTable = table;
         table = vector<HashNode<K, V>*>(nextPrime(2 * capacity), nullptr);
         size = 0;
@@ -118,87 +141,6 @@ public:
                 delete node;
             }
         }
-    }
-
-    void insert(const K& key, const V& value) {
-
-        if (static_cast<double>(size) / capacity > 0.7) {
-            rehash();
-        }
-
-        int probe = 0;
-        int hashIndex;
-        do {
-            hashIndex = getHashIndex(key, probe);
-            if (table[hashIndex] == nullptr || table[hashIndex]->isDeleted) {
-                if (table[hashIndex] == nullptr)
-                    table[hashIndex] = new HashNode<K, V>(key, value);
-                else {
-                    table[hashIndex]->key = key;
-                    table[hashIndex]->value = value;
-                    table[hashIndex]->isDeleted = false;
-                }
-                size++;
-                return;
-            }
-            probe++;
-        } while (probe < capacity);
-    }
-
-    V search(const K& key) {
-        int probe = 0;
-        int hashIndex;
-        while (probe < capacity) {
-            hashIndex = getHashIndex(key, probe);
-            if (table[hashIndex] == nullptr)
-                throw runtime_error("Key not found");
-            if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key)
-                return table[hashIndex]->value;
-            probe++;
-        }
-        throw runtime_error("Key not found");
-    }
-
-    void remove(const K& key) {
-        int probe = 0;
-        int hashIndex;
-        while (probe < capacity) {
-            hashIndex = getHashIndex(key, probe);
-            if (table[hashIndex] == nullptr)
-                return;
-            if (!table[hashIndex]->isDeleted && table[hashIndex]->key == key) {
-                table[hashIndex]->isDeleted = true;
-                size--;
-                return;
-            }
-            probe++;
-        }
-    }
-
-    ~HashTable() {
-        for (auto& node : table) {
-            if (node != nullptr) {
-                delete node;
-            }
-        }
-    }
-
-    int nextPrime(int n) {
-        if (n <= 1) return 2;
-        if (n <= 3) return 3;
-
-        if (n % 2 == 0) n++;
-
-        while (true) {
-            bool isPrime = true;
-            for (int i = 2; i * i <= n; i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) return n;
-            n += 2;
-        }
+        isRehashing = false;
     }
 };
